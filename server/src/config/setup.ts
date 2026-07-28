e422ac0d6c7506c2565d1e82c591dca07454de24
Farbod Ghasemi <farbod.ghasemi@edoramedia.com>
2026-07-28 15:13:15 +0330

    refactor: validate plugin config with zod instead of type assertions

--- server/src/config/setup.ts
+++ server/src/config/setup.ts
@@ -1,20 +1,12 @@
 import { Core } from '@strapi/strapi';
 
-import { isEmpty } from 'lodash';
 import configBase from '.';
 import {
   ConfigSchema,
-  NavigationItemAdditionalField,
   NavigationPluginConfigDBSchema,
   PluginConfigKeys,
   DynamicSchemas,
 } from '../schemas';
-import {
-  PluginConfigGraphQL,
-  PluginConfigNameFields,
-  PluginConfigPathDefaultFields,
-  PluginConfigPopulate,
-} from '../types';
 import { assertNotEmpty, resolveGlobalLikeId, validateAdditionalFields } from '../utils';
 
 type PluginDefaultConfigGetter = (
@@ -33,33 +25,32 @@ export const configSetup = async ({
     name: 'navigation',
   });
   const getFromPluginDefaults: PluginDefaultConfigGetter = await strapi.plugin('navigation').config;
-  const dbConfig = forceDefault
-    ? ({} as Partial<NavigationPluginConfigDBSchema>)
-    : (((await pluginStore.get({ key: 'config' })) ?? {}) as Partial<NavigationPluginConfigDBSchema>);
 
-  const configRaw = forceDefault
-    ? ({} as NavigationPluginConfigDBSchema)
-    : ({ ...configBase.default, ...dbConfig } as NavigationPluginConfigDBSchema);
+  const partialConfigSchema = DynamicSchemas.configSchema.partial();
+  const hardcodedDefaults = partialConfigSchema.parse(configBase.default);
+  const dbConfig = partialConfigSchema.parse(
+    forceDefault ? {} : ((await pluginStore.get({ key: 'config' })) ?? {})
+  );
 
-  if (!isEmpty(configRaw)) {
-    DynamicSchemas.configSchema.parse(configRaw);
-  }
+  const getWithFallback = getWithFallbackFactory(
+    dbConfig,
+    getFromPluginDefaults,
+    hardcodedDefaults
+  );
 
-  const getWithFallback = getWithFallbackFactory(dbConfig, getFromPluginDefaults);
-
-  const config: ConfigSchema = {
-    additionalFields: getWithFallback<NavigationItemAdditionalField[]>('additionalFields'),
-    contentTypes: getWithFallback<string[]>('contentTypes'),
-    contentTypesNameFields: getWithFallback<PluginConfigNameFields>('contentTypesNameFields'),
-    contentTypesPopulate: getWithFallback<PluginConfigPopulate>('contentTypesPopulate'),
-    defaultContentType: getWithFallback<string>('defaultContentType'),
-    allowedLevels: getWithFallback<number>('allowedLevels'),
-    gql: getWithFallback<PluginConfigGraphQL>('gql'),
-    pathDefaultFields: getWithFallback<PluginConfigPathDefaultFields>('pathDefaultFields'),
-    cascadeMenuAttached: getWithFallback<boolean>('cascadeMenuAttached'),
-    preferCustomContentTypes: getWithFallback<boolean>('preferCustomContentTypes'),
-    isCacheEnabled: getWithFallback<boolean>('isCacheEnabled'),
-  };
+  const config: ConfigSchema = DynamicSchemas.configSchema.parse({
+    additionalFields: getWithFallback('additionalFields'),
+    contentTypes: getWithFallback('contentTypes'),
+    contentTypesNameFields: getWithFallback('contentTypesNameFields'),
+    contentTypesPopulate: getWithFallback('contentTypesPopulate'),
+    defaultContentType: getWithFallback('defaultContentType'),
+    allowedLevels: getWithFallback('allowedLevels'),
+    gql: getWithFallback('gql'),
+    pathDefaultFields: getWithFallback('pathDefaultFields'),
+    cascadeMenuAttached: getWithFallback('cascadeMenuAttached'),
+    preferCustomContentTypes: getWithFallback('preferCustomContentTypes'),
+    isCacheEnabled: getWithFallback('isCacheEnabled'),
+  });
 
   handleDeletedContentTypes(config, { strapi });
 
@@ -74,16 +65,17 @@ export const configSetup = async ({
 };
 
 const getWithFallbackFactory =
-  (dbConfig: Partial<NavigationPluginConfigDBSchema>, fallback: PluginDefaultConfigGetter) =>
-  <T extends ReturnType<PluginDefaultConfigGetter>>(key: PluginConfigKeys) => {
-    const value =
-      dbConfig?.[key] ??
-      fallback(key) ??
-      (configBase.default as Record<string, unknown>)[key];
+  (
+    dbConfig: Partial<NavigationPluginConfigDBSchema>,
+    fallback: PluginDefaultConfigGetter,
+    hardcodedDefaults: Partial<NavigationPluginConfigDBSchema>
+  ) =>
+  (key: PluginConfigKeys) => {
+    const value = dbConfig[key] ?? fallback(key) ?? hardcodedDefaults[key];
 
     assertNotEmpty(value, new Error(`[Navigation] Config "${key}" is undefined`));
 
-    return value as T;
+    return value;
   };
 
 const handleDeletedContentTypes = (
